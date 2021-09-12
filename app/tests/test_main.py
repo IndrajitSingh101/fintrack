c9429481e11a006547f2f3d4d692f9f0c288349d
indrajitsingh <indrajit.distro@gmail.com>
2021-09-12 21:36:34 +0530

    new file added -pointed to aws

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -8,7 +8,3 @@ def test_main_resource():
     response_auth = client.get(f"/")
     assert response_auth.status_code == 200
 
-
-def test_child_resource():
-    response_auth = client.get(f"/api/v1/test")
-    assert response_auth.status_code == 200
